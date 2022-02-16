bbb6d4b8f9042ef77d47df699a609449d7017fbd
Andrew Stasiuk <awstasiuk@gmail.com>
2022-02-16 15:16:38 -0500

    first commit
    nice job andrew

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # mit-phd
-Collection of classes, functions, and methods written during my PhD
+Collection of classes, functions, and methods written during my (Andrew Stasiuk) PhD
+
+Install the required packages, listed in requirements.txt
+
+To install this package in developer mode, do a
+`py setup.py develop`
+and a
+`pip install -e .`
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mit-phd-awstasiuk",  # Replace with your own username
+    version="0.0.1",
+    author="Andrew Stasiuk",
+    author_email="astasiuk@mit.edu",
+    description="PhD Python stuff",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/awstasiuk/mit-phd",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
