43029972b77730645b495a52224260d07cc48980
Andrew Stasiuk <awstasiuk@gmail.com>
2022-02-28 10:50:21 -0500

    auto formatting with black
    yay

--- fermion/math.py
+++ fermion/math.py
@@ -1,18 +1,20 @@
 import numpy as np
 
+
 class Math:
     r"""
     A class which does math stuff
     """
+
     @staticmethod
-    def kron_delta(i,j):
-        if int(i) != i or int(j) !=j:
+    def kron_delta(i, j):
+        if int(i) != i or int(j) != j:
             raise ValueError("arguments should be integers")
-        return int(i==j)
+        return int(i == j)
 
     @staticmethod
     def chop(expr, delta=10**-10):
-        if hasattr(expr, '__iter__'):
+        if hasattr(expr, "__iter__"):
             return [Math.chop(x) for x in expr]
         else:
             return 0 if -delta <= abs(expr) <= delta else expr
--- fermion/operator.py
+++ fermion/operator.py
@@ -17,7 +17,11 @@ class Operator:
         if coef is not None:
             self._coef = coef
         else:
-            self._coef = {0:0, 1:np.zeros(2*n_fermion), 2:np.zeros((2*n_fermion,2*n_fermion))}
+            self._coef = {
+                0: 0,
+                1: np.zeros(2 * n_fermion),
+                2: np.zeros((2 * n_fermion, 2 * n_fermion)),
+            }
 
     def fourier_transform(self):
         r"""
@@ -25,18 +29,23 @@ class Operator:
         rewritten in the fermionic momentum basis. This can often block diagonalize
         Hamiltonians with periodic boundary conditions.
         """
-        n=self.n_fermion
+        n = self.n_fermion
         isqN = 1 / np.sqrt(n)
 
-        momenta = [(k-(n-1)/2) for k in range(n)]
-        f = np.array([[isqN * e ** (2j*pi*q*(i+1)/n) for q in momenta] for i in range(n)])
+        momenta = [(k - (n - 1) / 2) for k in range(n)]
+        f = np.array(
+            [
+                [isqN * e ** (2j * pi * q * (i + 1) / n) for q in momenta]
+                for i in range(n)
+            ]
+        )
 
-        F = np.block([[f,np.zeros((n,n))],[np.zeros((n,n)),np.conjugate(f)]])
+        F = np.block([[f, np.zeros((n, n))], [np.zeros((n, n)), np.conjugate(f)]])
 
-        coef = {0:self.coef[0]}
+        coef = {0: self.coef[0]}
         coef[1] = np.conjugate(F @ self.coef[1])
         coef[2] = np.conjugate(F.T) @ self.coef[2] @ F
-        return Operator(n,coef)
+        return Operator(n, coef)
 
     def normal_order(self, highest_order=2):
         r"""
@@ -51,11 +60,11 @@ class Operator:
         n = self.n_fermion
         for i in range(n):
             for j in range(n):
-                if mat[i,j] != 0:
-                    mat[j,i] += -mat[i+n,j+n]
-                    self._coef[0] += int(i==j) * mat[i+n,j+n]
-                    mat[i+n,j+n]=0
-        self._coef[2]=mat
+                if mat[i, j] != 0:
+                    mat[j, i] += -mat[i + n, j + n]
+                    self._coef[0] += int(i == j) * mat[i + n, j + n]
+                    mat[i + n, j + n] = 0
+        self._coef[2] = mat
 
     def jordan_wigner(self):
         r"""
@@ -68,28 +77,27 @@ class Operator:
 
         WARNING: Puts the operator into normal ordering
         """
-        if not np.allclose(self.coef[1],0):
+        if not np.allclose(self.coef[1], 0):
             raise ValueError("Not a quadratic fermionic operator")
         self.normal_order()
-        n=self.n_fermion
+        n = self.n_fermion
 
         # prepare the right matrix to diagonalize
-        alpha = self.coef[2][0:n,0:n]
-        beta = -2*np.conj(self.coef[2][0:n,n:2*n])
-        mat = (alpha-beta) @ (alpha + beta)
+        alpha = self.coef[2][0:n, 0:n]
+        beta = -2 * np.conj(self.coef[2][0:n, n : 2 * n])
+        mat = (alpha - beta) @ (alpha + beta)
 
         # diagonalize and extract the correctly ordered operators
         sqr_eig, phi = np.linalg.eigh(mat)
-        psi = [(1/np.sqrt(sqr_eig[i]))*(alpha-beta) @ phi[:,i] for i in range(n)]
+        psi = [(1 / np.sqrt(sqr_eig[i])) * (alpha - beta) @ phi[:, i] for i in range(n)]
         psi = np.array(psi)
         phi = phi.T
 
         # form the diagonalizing matrix, and the resulting diagonal operator.
-        G = 0.5*(phi+psi)
-        H = 0.5*(phi-psi)
+        G = 0.5 * (phi + psi)
+        H = 0.5 * (phi - psi)
         T = np.block([[G, H], [np.conj(H), np.conj(G)]])
-        return Operator.disorder_Z(n,-0.5*np.sqrt(sqr_eig)), T
-
+        return Operator.disorder_Z(n, -0.5 * np.sqrt(sqr_eig)), T
 
     def trace(self):
         r"""
@@ -97,17 +105,17 @@ class Operator:
         blowing up.
         """
         tr = self.coef[0]
-        tr += sum(self.coef[2][i,i] for i in range(2*self.n_fermion))
+        tr += sum(self.coef[2][i, i] for i in range(2 * self.n_fermion))
         return tr
 
-    def commutator(self,other):
+    def commutator(self, other):
         r"""
         computes the commutator of two operators, returns the resulting operator,
         C = [self, other].
         """
         return
 
-    def anti_commutator(self,other):
+    def anti_commutator(self, other):
         r"""
         computes the anit-commutator of two operators, returns the resulting operator,
         C = {self, other}.
@@ -123,26 +131,26 @@ class Operator:
         return self._coef
 
     @staticmethod
-    def quadratic_form(A,B):
+    def quadratic_form(A, B):
         n_fermion = len(A)
 
         if n_fermion != len(B):
             raise ValueError("Test")
-        shA=A.shape
-        shB=B.shape
-        if len(shA)!=2 and len(shB)!=2:
+        shA = A.shape
+        shB = B.shape
+        if len(shA) != 2 and len(shB) != 2:
             raise ValueError("test 2")
         if shA[0] != shA[1] or shB[0] != shB[1]:
             raise ValueError("test 3")
 
         q = Operator(n_fermion)
-        q._coef[2] = np.block([[A,-np.conjugate(B)],[B,-np.conjugate(A)]])
+        q._coef[2] = np.block([[A, -np.conjugate(B)], [B, -np.conjugate(A)]])
         return q
 
     @staticmethod
     def creation_op(index, n_spin):
         a = Operator(n_spin)
-        a._coef[1][index+n_spin] = 1
+        a._coef[1][index + n_spin] = 1
         return a
 
     @staticmethod
@@ -154,15 +162,15 @@ class Operator:
     @staticmethod
     def number_op(index, n_spin):
         N = Operator(n_spin)
-        N._coef[2][index,index] = 1
+        N._coef[2][index, index] = 1
         return N
 
     @staticmethod
     def global_Z(n_spin):
         Z = Operator(n_spin)
         for i in range(n_spin):
-            Z._coef[2][i,i]=-1
-            Z._coef[2][i+n_spin,i+n_spin]=1
+            Z._coef[2][i, i] = -1
+            Z._coef[2][i + n_spin, i + n_spin] = 1
         return Z
 
     @staticmethod
@@ -170,20 +178,20 @@ class Operator:
         if len(field) is not n_spin:
             raise ValueError("vector of disorder must equal number of spins")
         dZ = Operator(n_spin)
-        for idx,B in enumerate(field):
-            dZ._coef[2][idx,idx] = -B
-            dZ._coef[2][idx+n_spin,idx+n_spin] = B
+        for idx, B in enumerate(field):
+            dZ._coef[2][idx, idx] = -B
+            dZ._coef[2][idx + n_spin, idx + n_spin] = B
         return dZ
 
     @staticmethod
     def local_Z(index, n_spin):
         Zi = Operator(n_spin)
-        Zi._coef[2][index-1,index-1]=-1
-        Zi._coef[2][index-1+n_spin,index-1+n_spin]=1
+        Zi._coef[2][index - 1, index - 1] = -1
+        Zi._coef[2][index - 1 + n_spin, index - 1 + n_spin] = 1
         return Zi
 
     @staticmethod
-    def double_quantum(n_spin,B=0,J=1,periodic=False):
+    def double_quantum(n_spin, B=0, J=1, periodic=False):
         r"""
         Generate the nearest neighbor double quantum Hamiltonian quadratic form,
         `H=B*Sz + J*Sum(XX+YY)`
@@ -193,32 +201,34 @@ class Operator:
         conditions are set to be open. Periodic boundary conditions can be imposed
         by setting `periodic` to `True`.
         """
-        if hasattr(B, '__iter__'):
+        if hasattr(B, "__iter__"):
             A = np.diag(B)
         else:
-            A = -B*np.identity(n_spin)
+            A = -B * np.identity(n_spin)
 
-        C = J*(np.diag(np.ones(n_spin-1),1) - np.diag(np.ones(n_spin-1),-1))
+        C = J * (np.diag(np.ones(n_spin - 1), 1) - np.diag(np.ones(n_spin - 1), -1))
         if periodic:
             if n_spin % 2 == 0:
-                C[0,n_spin-1] = J
-                C[n_spin-1,0] = -J
+                C[0, n_spin - 1] = J
+                C[n_spin - 1, 0] = -J
             else:
-                C[0,n_spin-1] = -J
-                C[n_spin-1,0] = J
-        return Operator.quadratic_form(A,C)
+                C[0, n_spin - 1] = -J
+                C[n_spin - 1, 0] = J
+        return Operator.quadratic_form(A, C)
 
     def __add__(self, other):
-        if isinstance(other,Operator) and self.n_fermion == other.n_fermion:
+        if isinstance(other, Operator) and self.n_fermion == other.n_fermion:
             add = Operator(self.n_fermion)
             for i in range(3):
                 add._coef[i] = self.coef[i] + other.coef[i]
             return add
         else:
-            raise ValueError("Operator addition must be between ops with same number of fermions")
+            raise ValueError(
+                "Operator addition must be between ops with same number of fermions"
+            )
 
     def __mult__(self, other):
-        if isinstance(other,Operator):
+        if isinstance(other, Operator):
             raise ValueError("non-scalar multiplication not yet implemented")
         elif type(other) in (int, float, complex):
             mult = Operator(self.n_fermion)
