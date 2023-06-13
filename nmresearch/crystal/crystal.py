b56f9a98b04a89bee8d13d6f8b9e0a78f6e1f0b4
gaheller <gaheller@mit.edu>
2023-06-13 19:38:31 -0400

    changed crystalpy file
    changed crystal py file

--- nmresearch/crystal/crystal.py
+++ nmresearch/crystal/crystal.py
@@ -12,10 +12,16 @@ class Crystal:
     """
 
     def __init__(self, unit_cell, lattice):
-        self._unit_cell = unit_cell
-        self._lattice = lattice
+        self._unit_cell = unit_cell #list of atom positions in unit cell in cell basis
+        self._lattice = lattice #matrix converter between cell basis and real space, typically diagonal
 
     def generate_crystal(self, shell_radius=1):
+        shell_arr=[]
+        for a in range(-shell_radius, shell_radius+1):
+            for b in range(-shell_radius, shell_radius+1):
+                for c in range(-shell_radius, shell_radius+1):
+                    shell_arr.append([a,b,c])
+        return np.concatenate(shift(self._unit_cell, x) for x in shell_arr)
         r"""
         Generates a list of points for each `Atom` species in the lattice. `shell_radius`
         describes the number of unit cells
@@ -29,3 +35,9 @@ class Crystal:
     @property
     def lattice(self):
         return self._lattice
+    
+    def shift(cell, shift_vec):
+        return np.array([to_real_space(atom + shift_vec) for atom in cell])
+    
+    def to_real_space(vec):
+        return np.matmul(self._lattice, vec)
