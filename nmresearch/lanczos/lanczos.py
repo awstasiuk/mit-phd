e0d3b8c9fd958521b732a377a879167e47149b76
Andrew Stasiuk <awstasiuk@mit.edu>
2024-05-28 13:37:15 -0400

    lanczos class logic done

--- nmresearch/lanczos/lanczos.py
+++ nmresearch/lanczos/lanczos.py
@@ -1,6 +1,9 @@
 import numpy as np
 import scipy as sp
 from timeit import default_timer as timer
+import seaborn as sb
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
 
 from nmresearch.lanczos.utils import basis_vec
 
@@ -9,10 +12,14 @@ class Lanczos:
     def __init__(self, liouville, op):
         self.liouv = liouville
         self.op = op
-        self.e0 = None
-        self.L = None
         self.krylov_basis = None
         self.lanczos_coef = None
+        self.e0 = None
+        self.L = None
+        self.B = None
+        self.tbc_vals = None
+        self.k_complexity = None 
+       
 
     def compute_lanczos_fast(self,max_iter=50,tol=1e-10):
         r"""
@@ -91,6 +98,7 @@ class Lanczos:
             return
         if self.L is None:
             self.L = sp.sparse.diags([self.lanczos_coef,self.lanczos_coef], [1,-1])
+        if self.e0 is None:
             self.e0 = basis_vec(self.L.shape[0],0)
         return np.array([self.e0 @ (sp.sparse.linalg.expm_multiply(1j*self.L*t,self.e0)) for t in times])
     
@@ -108,3 +116,57 @@ class Lanczos:
         the timestep, for some reason.
         """
         return sp.sparse.linalg.expm_multiply(-1j*self.liouv, self.op, start=times[0], stop=times[-1], num=len(times), endpoint=True) @ self.op
+    
+    def orthogonality_test(self):
+        r"""
+        check the orthogonality of the krylov basis and generate a heatmap visualization of the resulting
+        matrix.
+        """
+        if self.krylov_basis is None:
+            print("Lanczos coefficients must be computed!")
+            return
+        n = len(self.krylov_basis)
+        orth_test =np.array([[self.krylov_basis[i].conj() @ self.krylov_basis[j] for i in range(n)] for j in range(n)])
+        ax = sb.heatmap(np.real(orth_test))
+        ax.invert_yaxis()
+        plt.xlabel("index 1")
+        plt.ylabel("index 2")
+        plt.title("Krylov Orthogonality test")
+        plt.show()
+
+    def tight_binding_complexity(self, start, end, max_step=.01, plot=True):
+        if self.lanczos_coef is None:
+            print("Lanczos coefficients must be computed!")
+            return
+        if self.B is None:
+            self.B = sp.sparse.diags([-1*self.lanczos_coef,self.lanczos_coef], [1,-1])
+        if self.e0 is None:
+            self.e0 = basis_vec(self.L.shape[0],0)
+
+        def f(t,y):
+            return self.B @ y
+        
+        self.tbc_vals = solve_ivp(f, (start, end), self.e0, max_step=.05)
+
+        if plot:
+            fig, ax = plt.subplots()
+            c = ax.pcolormesh(self.tbc_vals['t'], list(range(0,self.B.shape[0],1)), abs(self.tbc_vals['y'])**2)
+            ax.set_title("Tight-Binding Evolution")
+            # set the limits of the plot to the limits of the data
+            ax.axis([0,self.tbc_vals['t'][-1], 0, 20])
+            ax.set_ylabel('Complexity')
+            ax.set_xlabel('Time')
+            fig.colorbar(c, ax=ax)
+            plt.show()
+
+    def compute_krylov_complexity(self, plot=True):
+        if self.tbc_vals is None:
+            print("Solve the semi-infinite tight-binding model first")
+            return 
+        self.k_complexity = [np.sum((np.arange(0,self.B.shape[0],1)) * abs(self.tbc_vals['y'][:,idx])**2) for idx in range(len(self.tbc_vals['t']))]
+        
+        if plot:
+            plt.plot(self.tbc_vals['t'], self.k_complexity)
+            plt.xlabel("Time")
+            plt.ylabel("K-Complexity")
+            plt.show()
